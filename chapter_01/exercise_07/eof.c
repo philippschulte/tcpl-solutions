9b92ebe7bbcb3c3fee38d0a34d2708884917d471
philippschulte <philipp.schulte@ymail.com>
2019-07-14 13:35:20 -0700

    feat(chapter_01): add solution for exercise 1-7

---
+++ chapter_01/exercise_07/eof.c
@@ -0,0 +1,22 @@
+/****************************************************************************/                                                                            
+/* Exercise 1-7:                                                            */
+/*      Write a program to print the value of EOF.                          */
+/*                                                                          */
+/* Author: Philipp Schulte.                                                 */
+/*                                                                          */
+/* Purpose:                                                                 */
+/*      Learn what the value of EOF is on your system.                      */
+/*                                                                          */
+/* Usage:                                                                   */
+/*      Compile the program on your system. Run it and the value of EOF     */
+/*      appears.                                                            */
+/****************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Value of EOF: %d\n", EOF);
+    return 0;
+}
+
